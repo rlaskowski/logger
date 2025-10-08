70d101735c561148f80f31ac988dadcd7e81e93d
r.laskowski <rafal.laskowski@currenda.pl>
2025-10-08 15:03:32 +0200

    Project initiation

---
+++ logger.go
@@ -0,0 +1,97 @@
+package logger
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"strings"
+	"sync"
+)
+
+const (
+	Info  Level = "INFO"
+	Debug Level = "DEBUG"
+	Warn  Level = "WARN"
+	Err   Level = "ERROR"
+)
+
+type (
+	Logger struct {
+		mu     sync.Mutex
+		option *Option
+		writer io.Writer
+	}
+
+	Option struct {
+		level Level
+	}
+
+	OptionFn func(optio *Option)
+
+	Level string
+)
+
+func NewLogger(w io.Writer, options ...OptionFn) *Logger {
+	option := &Option{}
+	for _, opt := range options {
+		opt(option)
+	}
+	return &Logger{
+		mu:     sync.Mutex{},
+		writer: w,
+		option: option,
+	}
+}
+
+func WithLevel(level Level) OptionFn {
+	return func(option *Option) {
+		option.level = level
+	}
+}
+
+func (l *Logger) Info(msg ...any) {
+	l.build(Info, msg)
+}
+
+func (l *Logger) Debug(msg ...any) {
+	l.build(Debug, msg)
+}
+
+func (l *Logger) Warn(msg ...any) {
+	l.build(Warn, msg)
+}
+
+func (l *Logger) Err(msg ...any) {
+	l.build(Err, msg)
+}
+
+func (l *Logger) format(level Level, msg string) ([]byte, error) {
+	buf := &bytes.Buffer{}
+	if _, err := fmt.Fprintf(buf, "[%s] %s", string(level), msg); err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+func (l *Logger) build(level Level, msg ...any) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	if l.option.level != "" && l.option.level != level {
+		return
+	}
+	builder := strings.Builder{}
+	for _, d := range msg {
+		b, err := json.Marshal(d)
+		if err != nil {
+			return
+		}
+		builder.WriteString(string(b))
+	}
+	f, err := l.format(level, builder.String())
+	if err != nil {
+		return
+	}
+	fmt.Fprint(l.writer, string(f))
+}
---
+++ logger_test.go
@@ -0,0 +1,73 @@
+package logger
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBuild(t *testing.T) {
+	var (
+		buf    = bytes.Buffer{}
+		logger = NewLogger(&buf)
+	)
+
+	tests := []struct {
+		name,
+		want string
+		level Level
+		data  any
+	}{
+		{
+			name:  "info level",
+			level: Info,
+			data: struct {
+				Message string
+				Value   int
+			}{Message: "simple info message", Value: 11341},
+			want: `[INFO] {"Message":"simple info message","Value":11341}`,
+		},
+		{
+			name:  "warn level with short message",
+			level: Warn,
+			data:  "short message",
+			want:  `[WARN] "short message"`,
+		},
+		{
+			name:  "debug level",
+			level: Debug,
+			data: struct {
+				Name   string  `json:"firstName"`
+				Age    int     `json:"age"`
+				Amount float64 `json:"amount"`
+			}{
+				Name:   "John Carby",
+				Age:    45,
+				Amount: 120.32,
+			},
+			want: `[DEBUG] {"firstName":"John Carby","age":45,"amount":120.32}`,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			logger.build(test.level, test.data)
+			assert.Equal(t, test.want, buf.String())
+			buf.Reset()
+		})
+	}
+}
+
+func Test_withLevel(t *testing.T) {
+	var (
+		buf    = bytes.Buffer{}
+		logger = NewLogger(&buf, WithLevel(Level("DEBUG")))
+	)
+	msg := struct {
+		Message  string `json:"message"`
+		MetaData string `json:"metaData"`
+	}{Message: "simple test message", MetaData: "debug"}
+
+	logger.Info(msg)
+	assert.Empty(t, buf.String())
+}
